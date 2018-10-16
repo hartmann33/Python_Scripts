c8d712cae5f96b156c67c8ea135518e792c9ef71
Michael Hartmann <michaelhartmann33@gmail.com>
2018-10-15 22:04:02 -0700

    added a print line

--- main.py
+++ main.py
@@ -8,3 +8,5 @@ response = requests.get('https://httpbin.org/ip')
 print('Your IP is {0}'.format(response.json()['origin']))
 
 print(datetime.now())
+
+print("That was not as fast as I would have thought but oh well")
